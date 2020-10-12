9c7ca0d59be81d3fdf7eb904d2c706bd1ba58c74
mrashidz <rashid.z@gmail.com>
2020-10-12 16:02:30 +0200

    header added

---
+++ Day1/13_header/bar.h
@@ -0,0 +1,19 @@
+#ifndef BAR_H
+#define BAR_H
+#include <iostream>
+
+void appropriate1();
+void appropriate2(int a, int b);
+
+inline void advanceStuff() {
+    std::cout << "Inline functions would be inserted wherever used!" << std::endl;
+}
+
+/*
+void badWayOfDoingThings(int a, int b) {
+    if (a > b) std::cout << a << " is not bigget than " << b << std::endl;
+    else std::cout << a << " is not bigget than " << b << std::endl;
+}
+*/
+#endif // BAR_H
+
