620dd9f0302015092abae4661cbfb9d052e615e2
mrashidz <rashid.z@gmail.com>
2020-10-12 13:47:31 +0200

    functions

--- Day1/3_string/st.cpp
+++ Day1/3_string/st.cpp
@@ -2,7 +2,10 @@
 
 
 int main() {
-    char a[3] = "abc";
+    char a[3] = "bc";
+    char b[3] = "YZ";
+    std::cout << a << std::endl;
+    a[2] = 'a';
     std::cout << a << std::endl;
     std::string a_string = "abc";
     return 0;
