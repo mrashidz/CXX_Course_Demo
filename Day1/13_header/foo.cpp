03b84f0bf9eafcdb48b18bda3be17677d4fe4441
mrashidz <rashid.z@gmail.com>
2020-10-12 16:02:03 +0200

    First Day Done!

---
+++ Day1/13_header/foo.cpp
@@ -0,0 +1,17 @@
+#include "bar.h"
+
+int main() {
+
+    appropriate1();
+    appropriate2(3, 3);
+
+    advanceStuff();
+
+
+    //badWayOfDoingThings(4, 8);
+
+    return 0;
+}
+
+
+
---
+++ Day1/14_cast/cast.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+
+    
+    float  f = 33.21;
+    int    i = -10;
+
+    
+    float ff  = i;
+    int ii = f;
+    char c = i;
+    unsigned ui = c;
+    unsigned char uc = ui;
+    
+
+
+    float fff = i/ii;
+    // C-Style Casting
+    float ffff = (float) i/ii;
+    
+    std::cout << "i: " << i <<  " c: " << c << " c value: " << static_cast<int> (c) << std::endl;
+    std::cout << "ui: " << ui << " uc: " << uc << " uc value: " << static_cast<int> (uc)<< std::endl;
+    std::cout << "ff: " << f << " ii: " << ii << std::endl;    
+    std::cout << "fff: " << fff << " ffff: " << ffff << std::endl;
+
+    return 0;
+}
